5821a60ba09328869641061a077fdf3225ad5faf
Masudur-Rahman21 <tayef@gmail.com>
2022-02-08 21:06:29 +0600

    hi

---
+++ MyJs/Home_js.js
@@ -0,0 +1,12 @@
+/*
+  var dropdownItem = document.querySelector('.dropdown-item');
+  var dropdown = document.querySelector('.dropdown');
+
+  dropdownItem.addEventListener('mouseenter', function(e) {
+    dropdown.classList.add('dropdown-show');
+  });
+  dropdownItem.addEventListener('mouseleave', function() {
+    dropdown.classList.remove('dropdown-show');
+  })
+*/
+
